c13ec5a97d3bb2cabd37c6b3055393363746b561
Bastian Kersting <bastian@cmbt.de>
2020-12-31 13:59:40 +0100

    Added locking; poll is now a loop

--- src/transport.rs
+++ src/transport.rs
@@ -1,17 +1,23 @@
-use crate::packet::{Error, Packet, PacketId, decode_payload, encode_payload};
+use crate::packet::{decode_payload, encode_payload, Error, Packet, PacketId};
 use crypto::{digest::Digest, sha1::Sha1};
+
 use rand::{thread_rng, Rng};
 use reqwest::{Client, Url};
 use serde::{Deserialize, Serialize};
-use std::sync::{atomic::AtomicBool, Arc};
+use std::{
+    sync::{atomic::AtomicBool, Arc, Mutex},
+    time::Instant,
+};
 
+#[derive(Debug, Clone)]
 enum TransportType {
-    Polling(Client),
+    Polling(Arc<Mutex<Client>>),
 }
 
 // do we might need a lock here? -> I would say yes, at least for message events
-type Callback<I> = Arc<Option<Box<dyn Fn(I)>>>;
+type Callback<I> = Arc<Mutex<Option<Box<dyn Fn(I)>>>>;
 
+#[derive(Clone)]
 struct TransportClient {
     transport: TransportType,
     on_error: Callback<String>,
@@ -20,11 +26,13 @@ struct TransportClient {
     on_data: Callback<Vec<u8>>,
     on_packet: Callback<Packet>,
     connected: Arc<AtomicBool>,
-    address: Option<String>,
+    last_ping: Arc<Mutex<Instant>>,
+    last_pong: Arc<Mutex<Instant>>,
+    host_address: Arc<Mutex<Option<String>>>,
     connection_data: Option<HandshakeData>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct HandshakeData {
     sid: String,
     upgrades: Vec<String>,
@@ -34,17 +42,21 @@ struct HandshakeData {
     ping_timeout: i32,
 }
 
+unsafe impl Send for TransportClient {}
+
 impl TransportClient {
     pub fn new() -> Self {
         TransportClient {
-            transport: TransportType::Polling(Client::new()),
-            on_error: Arc::new(None),
-            on_open: Arc::new(None),
-            on_close: Arc::new(None),
-            on_data: Arc::new(None),
-            on_packet: Arc::new(None),
+            transport: TransportType::Polling(Arc::new(Mutex::new(Client::new()))),
+            on_error: Arc::new(Mutex::new(None)),
+            on_open: Arc::new(Mutex::new(None)),
+            on_close: Arc::new(Mutex::new(None)),
+            on_data: Arc::new(Mutex::new(None)),
+            on_packet: Arc::new(Mutex::new(None)),
             connected: Arc::new(AtomicBool::default()),
-            address: None,
+            last_ping: Arc::new(Mutex::new(Instant::now())),
+            last_pong: Arc::new(Mutex::new(Instant::now())),
+            host_address: Arc::new(Mutex::new(None)),
             connection_data: None,
         }
     }
@@ -64,16 +76,22 @@ impl TransportClient {
                 )[..];
 
                 if let Ok(full_address) = Url::parse(&(address.clone() + query_path)[..]) {
-                    self.address = Some(address);
+                    self.host_address = Arc::new(Mutex::new(Some(address)));
 
-                    let response = dbg!(client.get(full_address).send().await?.text().await?);
+                    let client = client.lock().unwrap();
+                    let response = client.get(full_address).send().await?.text().await?;
+                    drop(client);
 
                     if let Ok(connection_data) = serde_json::from_str(&response[1..]) {
                         self.connection_data = dbg!(connection_data);
 
-                        if let Some(function) = self.on_open.as_ref() {
+                        let function = self.on_open.lock().unwrap();
+                        if let Some(function) = function.as_ref() {
                             function(());
                         }
+                        drop(function);
+
+                        *Arc::get_mut(&mut self.connected).unwrap() = AtomicBool::from(true);
                         return Ok(());
                     }
                     return Err(Error::HandshakeError(response));
@@ -84,7 +102,7 @@ impl TransportClient {
     }
 
     pub async fn emit(&mut self, packet: Packet) -> Result<(), Error> {
-        if self.connected.load(std::sync::atomic::Ordering::Relaxed) {
+        if !self.connected.load(std::sync::atomic::Ordering::Relaxed) {
             return Err(Error::ActionBeforeOpen);
         }
         match &mut self.transport {
@@ -95,11 +113,13 @@ impl TransportClient {
                     self.connection_data.as_ref().unwrap().sid
                 );
 
+                let host = self.host_address.lock().unwrap();
                 let address =
-                    Url::parse(&(self.address.as_ref().unwrap().to_owned() + query_path)[..])
-                        .unwrap();
+                    Url::parse(&(host.as_ref().unwrap().to_owned() + query_path)[..]).unwrap();
+                drop(host);
 
                 let data = encode_payload(vec![packet]);
+                let client = client.lock().unwrap();
                 let status = client
                     .post(address)
                     .body(data)
@@ -107,6 +127,7 @@ impl TransportClient {
                     .await?
                     .status()
                     .as_u16();
+                drop(client);
                 if status != 200 {
                     return Err(Error::HttpError(status));
                 }
@@ -116,65 +137,106 @@ impl TransportClient {
         }
     }
 
-    async fn poll(&mut self) -> Result<(), Error> {
-        if self.connected.load(std::sync::atomic::Ordering::Relaxed) {
+    async fn poll_cycle(&mut self) -> Result<(), Error> {
+        if !self.connected.load(std::sync::atomic::Ordering::Relaxed) {
             return Err(Error::ActionBeforeOpen);
         }
+        println!("Here");
 
-        match &mut self.transport {
-            TransportType::Polling(client) => {
-                let query_path = &format!(
-                    "/engine.io/?EIO=4&transport=polling&t={}&sid={}",
-                    TransportClient::get_random_t(),
-                    self.connection_data.as_ref().unwrap().sid
-                );
+        while self.connected.load(std::sync::atomic::Ordering::Relaxed) {
+            match &mut self.transport {
+                TransportType::Polling(client) => {
+                    let query_path = &format!(
+                        "/engine.io/?EIO=4&transport=polling&t={}&sid={}",
+                        TransportClient::get_random_t(),
+                        self.connection_data.as_ref().unwrap().sid
+                    );
 
-                let address = Url::parse(&(self.address.as_ref().unwrap().to_owned() + query_path)[..])
-                .unwrap();
+                    let host = self.host_address.lock().unwrap();
+                    let address =
+                        Url::parse(&(host.as_ref().unwrap().to_owned() + query_path)[..]).unwrap();
+                    drop(host);
 
-                // TODO: check if to_vec is inefficient here
-                let response = client.get(address).send().await?.bytes().await?.to_vec();
-                let packets = decode_payload(response)?;
-                for packet in packets {
-                    // call the packet callback
-                    if let Some(function) = self.on_packet.as_ref() {
-                        function(packet.clone());
+                    let client = client.lock().unwrap();
+                    // TODO: check if to_vec is inefficient here
+                    let response = client.get(address).send().await?.bytes().await?.to_vec();
+                    drop(client);
+                    let packets = decode_payload(response)?;
+
+                    for packet in packets {
+                        let on_packet = self.on_packet.lock().unwrap();
+                        // call the packet callback
+                        // TODO: execute this in a new thread
+                        if let Some(function) = on_packet.as_ref() {
+                            function(packet.clone());
+                        }
+                        drop(on_packet);
+
+                        // check for the appropiate action or callback
+                        match packet.packet_id {
+                            PacketId::Message => {
+                                let on_data = self.on_data.lock().unwrap();
+                                // TODO: execute this in a new thread
+                                if let Some(function) = on_data.as_ref() {
+                                    function(packet.data);
+                                }
+                                drop(on_data);
+                            }
+
+                            PacketId::Close => {
+                                dbg!("Received close!");
+                                *Arc::get_mut(&mut self.connected).unwrap() =
+                                    AtomicBool::from(false);
+                                let on_close = self.on_close.lock().unwrap();
+                                if let Some(function) = on_close.as_ref() {
+                                    function(());
+                                }
+                                drop(on_close);
+                            }
+                            PacketId::Open => {
+                                // this will never happen as the client connects to the server and the open packet
+                                // is already received in the 'open' method
+                                unreachable!();
+                            }
+                            PacketId::Upgrade => {
+                                dbg!("Received upgrade!");
+                                todo!("Upgrade the connection, but only if possible");
+                            }
+                            PacketId::Ping => {
+                                dbg!("Received ping!");
+
+                                let mut last_ping = self.last_ping.lock().unwrap();
+                                *last_ping = Instant::now();
+                                drop(last_ping);
+
+                                self.emit(Packet::new(PacketId::Pong, Vec::new())).await?;
+
+                                let mut last_pong = self.last_pong.lock().unwrap();
+                                *last_pong = Instant::now();
+                                drop(last_pong);
+                            }
+                            PacketId::Pong => {
+                                // this will never happen as the pong packet is only sent by the client
+                                unreachable!();
+                            }
+                            PacketId::Noop => (),
+                        }
                     }
 
-                    // check for the appropiate action or callback
-                    match packet.packet_id {
-                        PacketId::Message => {
-                            if let Some(function) = self.on_data.as_ref() {
-                                function(packet.data);
-                            }
-                        }
-                        PacketId::Close => {
-                            dbg!("Received close!");
-                            todo!("Close the connection");
-                        }
-                        PacketId::Open => {
-                            dbg!("Received open!");
-                            todo!("Think about this, we just receive it in the 'open' method.")
-                        }
-                        PacketId::Upgrade => {
-                            dbg!("Received upgrade!");
-                            todo!("Upgrade the connection, but only if possible");
-                        }
-                        PacketId::Ping => {
-                            dbg!("Received ping!");
-                            todo!("Update ping state and send pong");
-                        }
-                        PacketId::Pong => {
-                            dbg!("Received pong!");
-                            todo!("Won't really happen, just the server sends those");
-                        }
-                        PacketId::Noop => ()
+                    let last_pong = self.last_pong.lock().unwrap();
+                    let is_server_inactive = last_pong.elapsed().as_millis()
+                        > self.connection_data.as_ref().unwrap().ping_timeout as u128;
+                    drop(last_pong);
+
+                    if is_server_inactive {
+                        // server is unresponsive, send close packet and close connection
+                        self.emit(Packet::new(PacketId::Close, Vec::new())).await?;
+                        *Arc::get_mut(&mut self.connected).unwrap() = AtomicBool::from(false);
                     }
                 }
-
-                Ok(())
             }
         }
+        return Ok(());
     }
 
     // Produces a random String that is used to prevent browser caching.
@@ -191,7 +253,6 @@ impl TransportClient {
 #[cfg(test)]
 mod test {
     use crate::packet::PacketId;
-    use std::str;
 
     use super::*;
 
@@ -211,10 +272,10 @@ mod test {
             .await
             .unwrap();
 
-        socket.on_data = Arc::new(Some(Box::new(|data| {
-            println!("Received: {:?}", str::from_utf8(&data).unwrap());
-        })));
- 
-        socket.poll().await.unwrap();
+        socket.on_data = Arc::new(Mutex::new(Some(Box::new(|data| {
+            println!("Received: {:?}", std::str::from_utf8(&data).unwrap());
+        }))));
+
+        socket.poll_cycle().await.unwrap();
     }
 }
