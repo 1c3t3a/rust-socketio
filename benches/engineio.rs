028eff5ea384d91379f5bdaf25786d0b1d250527
Nathan Shaaban <nshaaban@cpacketnetworks.com>
2021-08-05 17:48:29 +0000

    formatting

--- benches/engineio.rs
+++ benches/engineio.rs
@@ -1,15 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rust_socketio::engineio::{
-    socket::{Socket, SocketBuilder},
-    transport::Transport,
     packet::{Packet, PacketId},
-    transports::polling::PollingTransport,
+    transport::TransportClient
 };
 use bytes::Bytes;
 use rust_socketio::error::Error;
-use url::Url;
 
-fn engine_io_emit<T: Transport>(socket: &Socket<T>, packet: Packet) -> Result<(), Error> {
+fn engine_io_emit(socket: &TransportClient, packet: Packet) -> Result<(), Error> {
     let mut i = 0;
     while i < 100 {
         i+=1;
@@ -18,14 +15,16 @@ fn engine_io_emit<T: Transport>(socket: &Socket<T>, packet: Packet) -> Result<()
     Ok(())
 }
 
-fn engine_io_url() -> Result<Url, Error> {
+fn engine_io_url() -> Result<String, Error> {
     const SERVER_URL: &str = "http://localhost:4201";
     let url = std::env::var("ENGINE_IO_SERVER").unwrap_or_else(|_| SERVER_URL.to_owned());
-    Ok(Url::parse(&url)?)
+    Ok(url)
 }
 
-fn engine_io_socket_build(url: Url) -> Result<Socket<PollingTransport>, Error> {
-    SocketBuilder::new(url).build()
+fn engine_io_socket_build(url: String) -> TransportClient {
+    let mut client = TransportClient::new(true, None, None);
+    client.open(url).unwrap();
+    client
 }
 
 fn engine_io_packet() -> Packet {
@@ -47,8 +46,7 @@ fn criterion_engine_io_packet(c: &mut Criterion) {
 
 fn criterion_engine_io_emit(c: &mut Criterion) {
     let url = engine_io_url().unwrap();
-    let mut socket = engine_io_socket_build(url).unwrap();
-    socket.connect().unwrap();
+    let socket = engine_io_socket_build(url);
     let packet = engine_io_packet();
 
     c.bench_function("engine io emit", |b| b.iter(|| engine_io_emit(black_box(&socket), black_box(packet.clone())).unwrap()));
