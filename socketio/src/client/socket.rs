4aab6ce7e687a9ff42f47cc6bf29bc0c0dd6ad86
Nathan Shaaban <nshaaban@cpacketnetworks.com>
2021-08-26 19:32:20 +0000

    feat: transport type option exposed

--- socketio/src/client/socket.rs
+++ socketio/src/client/socket.rs
@@ -1,7 +1,9 @@
 pub use super::super::{event::Event, payload::Payload};
 use crate::packet::Packet as SocketPacket;
 use native_tls::TlsConnector;
+use rust_engineio::client::{Socket as EngineIoSocket, SocketBuilder as EngineIoSocketBuilder};
 use rust_engineio::header::{HeaderMap, HeaderValue};
+use url::Url;
 
 use crate::error::Result;
 use std::{time::Duration, vec};
@@ -20,6 +22,16 @@ pub struct Socket {
 
 type SocketCallback = dyn FnMut(Payload, Socket) + 'static + Sync + Send;
 
+/// Flavor of engineio transport
+//TODO: Consider longevity
+#[derive(Clone, Eq, PartialEq)]
+pub enum TransportType {
+    Any,
+    Websocket,
+    WebsocketUpgrade,
+    Polling,
+}
+
 /// A builder class for a `socket.io` socket. This handles setting up the client and
 /// configuring the callback, the namespace and metadata of the socket. If no
 /// namespace is specified, the default namespace `/` is taken. The `connect` method
@@ -30,6 +42,7 @@ pub struct SocketBuilder {
     namespace: Option<String>,
     tls_config: Option<TlsConnector>,
     opening_headers: Option<HeaderMap>,
+    transport_type: TransportType,
 }
 
 impl SocketBuilder {
@@ -70,6 +83,7 @@ impl SocketBuilder {
             namespace: None,
             tls_config: None,
             opening_headers: None,
+            transport_type: TransportType::Any,
         }
     }
 
@@ -169,6 +183,14 @@ impl SocketBuilder {
         self
     }
 
+    /// Specifies which underlying transport type to use
+    // TODO: better docs
+    pub fn transport_type(mut self, transport_type: TransportType) -> Self {
+        self.transport_type = transport_type;
+
+        self
+    }
+
     /// Connects the socket to a certain endpoint. This returns a connected
     /// [`Socket`] instance. This method returns an [`std::result::Result::Err`]
     /// value if something goes wrong during connection.
@@ -192,13 +214,29 @@ impl SocketBuilder {
     /// assert!(result.is_ok());
     /// ```
     pub fn connect(self) -> Result<Socket> {
-        //TODO: Add check for empty path and set to /socket.io/
-        let mut socket = Socket::new(
-            self.address,
-            self.namespace,
-            self.tls_config,
-            self.opening_headers,
-        )?;
+        let mut url = Url::parse(&self.address)?;
+
+        if url.path() == "/" {
+            url.set_path("/socket.io/");
+        }
+
+        let mut builder = EngineIoSocketBuilder::new(url);
+
+        if let Some(tls_config) = self.tls_config {
+            builder = builder.tls_config(tls_config);
+        }
+        if let Some(headers) = self.opening_headers {
+            builder = builder.headers(headers);
+        }
+
+        let socket = match self.transport_type {
+            TransportType::Any => builder.build_with_fallback()?,
+            TransportType::Polling => builder.build_polling()?,
+            TransportType::Websocket => builder.build_websocket()?,
+            TransportType::WebsocketUpgrade => builder.build_websocket_with_upgrade()?,
+        };
+
+        let mut socket = Socket::new_with_socket(self.address, self.namespace, socket)?;
         if let Some(callbacks) = self.on {
             for (event, callback) in callbacks {
                 socket.on(event, Box::new(callback)).unwrap();
@@ -222,6 +260,16 @@ impl Socket {
     ) -> Result<Self> {
         Ok(Socket {
             socket: InnerSocket::new(address, namespace, tls_config, opening_headers)?,
+        })
+    }
+
+    pub(crate) fn new_with_socket<T: Into<String>>(
+        address: T,
+        namespace: Option<String>,
+        engine_socket: EngineIoSocket,
+    ) -> Result<Self> {
+        Ok(Socket {
+            socket: InnerSocket::new_with_socket(address, namespace, engine_socket)?,
         })
     }
 
@@ -446,7 +494,7 @@ mod test {
         let url = crate::test::socket_io_server()?;
 
         // test socket build logic
-        let socket_builder = SocketBuilder::new(url);
+        let socket_builder = SocketBuilder::new(url.clone());
 
         let tls_connector = TlsConnector::builder()
             .use_sni(true)
@@ -460,11 +508,8 @@ mod test {
             .opening_header("accept-encoding", "application/json")
             .on("test", |str, _| println!("Received: {:#?}", str))
             .on("message", |payload, _| println!("{:#?}", payload))
-            .connect();
+            .connect()?;
 
-        assert!(socket.is_ok());
-
-        let socket = socket.unwrap();
         assert!(socket.emit("message", json!("Hello World")).is_ok());
 
         assert!(socket.emit("binary", Bytes::from_static(&[46, 88])).is_ok());
@@ -479,6 +524,19 @@ mod test {
             .is_ok());
 
         sleep(Duration::from_secs(5));
+
+        SocketBuilder::new(url.clone())
+            .transport_type(TransportType::Polling)
+            .connect()?;
+        SocketBuilder::new(url.clone())
+            .transport_type(TransportType::Websocket)
+            .connect()?;
+        SocketBuilder::new(url.clone())
+            .transport_type(TransportType::WebsocketUpgrade)
+            .connect()?;
+        SocketBuilder::new(url.clone())
+            .transport_type(TransportType::Any)
+            .connect()?;
 
         Ok(())
     }
