d2b0ed7edc2a7ef43ee602e3ad3990e230fa3cc8
Nathan Shaaban <nshaaban@cpacketnetworks.com>
2021-09-08 18:37:38 +0000

    feat: add polling engineio

---
+++ ci/engine-io-polling.js
@@ -0,0 +1,44 @@
+/**
+ * This is an example server, used to test the current code.
+ */
+const engine = require('engine.io');
+const http = require('http').createServer().listen(4203);
+// the engine.io client runs on port 4203
+const server = engine.attach(http, {
+    allowUpgrades: false
+});
+
+console.log("Started")
+server.on('connection', socket => {
+    console.log("Connected");
+
+    socket.on('message', message => {
+        if (message !== undefined) {
+            console.log(message.toString());
+            if (message == "respond") {
+                socket.send("Roger Roger");
+            } else if (message == "close") {
+                socket.close();
+            }
+        } else {
+            console.log("empty message received")
+        }
+    });
+
+    socket.on('heartbeat', () => {
+        console.log("heartbeat");
+    });
+
+    socket.on('error', message => {
+        // Notify the client if there is an error so it's tests will fail
+        socket.send("ERROR: Received error")
+        console.log(message.toString());
+    });
+
+    socket.on('close', () => {
+        console.log("Close");
+        socket.close();
+    });
+
+    socket.send('hello client');
+});
