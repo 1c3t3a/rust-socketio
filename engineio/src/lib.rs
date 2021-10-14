bdbbee23d7a2be353ee99ee970baa3b7f3a0ec74
Bastian Kersting <bastian@cmbt.de>
2021-10-14 14:52:33 +0200

    doc: Added documentation for the engineio module

--- engineio/src/lib.rs
+++ engineio/src/lib.rs
@@ -1,38 +1,52 @@
-//! rust_engineio is a engine.io client written in the Rust Programming Language.
+//! # Rust-engineio-client
+//!
+//! An implementation of a engine.io client written in the rust programming language. This implementation currently
+//! supports revision 4 of the engine.io protocol. If you have any connection issues with this client,
+//! make sure the server uses at least revision 4 of the engine.io protocol.
+//!
 //! ## Example usage
 //!
-//TODO: 0.3.X missing docs
 //! ``` rust
+//! use rust_engineio::{ClientBuilder, Client, packet::{Packet, PacketId}};
+//! use url::Url;
+//! use Bytes;
 //!
+//! // get a client that is connected to the server
+//! let client: Client = ClientBuilder::new(Url::parse("http://localhost:4201").unwrap())
+//!      .on_open(|_| println!("Connection opened!"))
+//!      .build()
+//!      .expect("Connection failed");
+//!
+//! // create a packet, in this case a message packet and emit it
+//! let packet = Packet::new(PacketId::Message, &b"Hello World");
+//! client.emit(packet).expect("Server unreachable");
+//!
+//! // disconnect from the server
+//! client.disconnect().expect("Disconnect failed")
 //! ```
 //!
 //! The main entry point for using this crate is the [`ClientBuilder`] which provides
-//! a way to easily configure a socket in the needed way. When the `connect` method
-//! is called on the builder, it returns a connected client which then could be used
-//! to emit messages to certain events. One client can only be connected to one namespace.
-//! If you need to listen to the messages in different namespaces you need to
-//! allocate multiple sockets.
+//! the opportunity to define how you want to connect to a certain endpoint.
+//! The following connection methods are available:
+//! * `build`: Build websocket if allowed, if not fall back to polling. Standard configuration.
+//! * `build_polling`: enforces a `polling` transport.
+//! * `build_websocket_with_upgrade`: Build socket with a polling transport then upgrade to websocket transport (if possible).
+//! * `build_websocket`: Build socket with only a websocket transport, crashes when websockets are not allowed.
+//!
 //!
 //! ## Current features
 //!
-//! This implementation now supports all of the features of the socket.io protocol mentioned
-//! [here](https://github.com/socketio/socket.io-protocol).
-//! It generally tries to make use of websockets as often as possible. This means most times
-//! only the opening request uses http and as soon as the server mentions that he is able to use
-//! websockets, an upgrade  is performed. But if this upgrade is not successful or the server
-//! does not mention an upgrade possibility, http-long polling is used (as specified in the protocol specs).
+//! This implementation now supports all of the features of the engine.io protocol mentioned [here](https://github.com/socketio/engine.io-protocol).
+//! This includes various transport options, the possibility of sending engine.io packets and registering the
+//! commmon engine.io event callbacks:
+//! * on_open
+//! * on_close
+//! * on_data
+//! * on_error
+//! * on_packet
 //!
-//! Here's an overview of possible use-cases:
-//!
-//! - connecting to a server.
-//! - register callbacks for the following event types:
-//!     - open
-//!     - close
-//!     - error
-//!     - message
-//! - send JSON data to the server (via `serde_json` which provides safe
-//! handling).
-//! - send and handle Binary data.
+//! It is also possible to pass in custom tls configurations via the `TlsConnector` as well
+//! as custom headers for the opening request.
 //!
 #![allow(clippy::rc_buffer)]
 #![warn(clippy::complexity)]
