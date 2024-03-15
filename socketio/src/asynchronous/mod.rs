332d2dc5d45f326dfc19a82a049d3f31e3d77862
shenjack <3695888@qq.com>
2024-03-15 23:56:37 +0800

    Add async_any_callback and async_callback macro with it's docs

--- socketio/src/asynchronous/mod.rs
+++ socketio/src/asynchronous/mod.rs
@@ -4,5 +4,49 @@ mod socket;
 
 #[cfg(feature = "async")]
 pub use client::builder::ClientBuilder;
-pub use client::client::Client;
-pub use client::client::ReconnectSettings;
+pub use client::client::{Client, ReconnectSettings};
+
+
+#[doc = r#"
+A macro to wrap a async callback function to be used in the client.
+
+This macro is used to wrap a callback function that can handle any event.
+
+```rust
+pub async fn callback(payload: Payload, client: Client) {}
+
+let socket = ClientBuilder::new(host)
+        .on("message", async_callback!(callback))
+        .connect()
+        .await
+        .expect("Connection failed");
+```
+"#]
+#[macro_export]
+macro_rules! async_callback {
+    ($f:expr) => {
+        |payload: Payload, client: Client| $f(payload, client).boxed()
+    };
+}
+
+#[doc = r#"
+A macro to wrap a async callback function to be used in the client.
+
+This macro is used to wrap a callback function that can handle any event.
+
+```rust
+pub async fn callback_any(event: Event, payload: Payload, client: Client) {}
+
+let socket = ClientBuilder::new(host)
+        .on_any(async_any_callback!(callback_any))
+        .connect()
+        .await
+        .expect("Connection failed");
+```
+"#]
+#[macro_export]
+macro_rules! async_any_callback {
+    ($f:expr) => {
+        |event: Event, payload: Payload, client: Client| $f(event, payload, client).boxed()
+    };
+}
